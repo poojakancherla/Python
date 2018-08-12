3f4274a65c0e32b77c21f36845aea71c71147717
Pooja Kancherla <pknq6@mst.edu>
2018-08-12 16:44:39 -0500

    Compressing a string

---
+++ StringCompression.py
@@ -0,0 +1,18 @@
+def compress(s):
+    if len(s) == 0:
+        return ''
+    else:
+        s_compress = ""
+        curr = s[0]
+        count = 1
+        for i in s[1:]:
+            if i == curr:
+                count += 1
+
+            else:
+                s_compress += curr + str(count)
+                count = 1
+                curr = i
+    return s_compress +curr + str(count)
+
+print(compress(''))
