b03906e7006f3105f919b853b0b78eee01343472
Pooja Kancherla <pknq6@mst.edu>
2018-08-13 23:12:04 -0500

    Implementation of Queue

---
+++ Queue.py
@@ -0,0 +1,19 @@
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+obj = Queue()
+obj.enqueue(2)
+print(obj.dequeue())
