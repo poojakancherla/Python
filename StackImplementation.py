804c50132033d919d9c603dea320f5011dd6169e
Pooja Kancherla <pknq6@mst.edu>
2018-08-13 20:17:18 -0500

    Implementation of a stack with some of its basic operations

---
+++ StackImplementation.py
@@ -0,0 +1,22 @@
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+s = Stack()
+s.push(6)
+print(s.peek())
