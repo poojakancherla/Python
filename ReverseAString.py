77239d47b3085db419549ea06877cd8933459f80
Pooja Kancherla <pknq6@mst.edu>
2018-08-29 19:06:08 -0500

    Modified by performing recursion first and then add the elements starting from index 0

--- ReverseAString.py
+++ ReverseAString.py
@@ -1,9 +1,9 @@
 #  Reverse a string using recursion
 
 def reverse(s):
-    if len(s) == 1:
+    if len(s) <= 1:
         return s
     else:
-        return s[-1] + reverse(s[:-1])
+        return reverse(s[1:]) + s[0]
 
 print(reverse('hello world'))
