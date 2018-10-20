8061e768d8a226dfde65a5129a33534c32da490a
poojakancherla <pknq6@mst.edu>
2018-10-20 09:31:16 -0500

    Level order trsversal without the implementation of queue

--- LevelOrderTraversal.py
+++ LevelOrderTraversal.py
@@ -28,33 +28,20 @@ insert(head, Node(3))
 insert(head, Node(5))
 insert(head, Node(7))
 
-class queue():
-    def __init__(self):
-        self.items = []
-    def is_empty(self):
-        return self.items == []
-    def enqueue(self, item):
-        self.items.insert(0, item)
-    def dequeue(self):
-        return self.items.pop()
-
-
 
 def LevelOrder(root):
-    #  Create a queue
-    q = queue()
     if root is None:
         print('Nothing')
-    # Add the nde to the queue
-    q.enqueue(root)
-    while(q.is_empty() == False): # while the queue has elements
-        root = q.dequeue() # Remove the first element and set it as the root
+    # Add the node to a queue
+    q = [root]
+    while q: # while the queue has elements
+        root = q.pop() # Remove the first element and set it as the root
         print(root.val, end=' ') # print it
 
         # Process next level
         if root.left: # If the root has left child
-            q.enqueue(root.left) # Add it to the queue
+            q.insert(0, root.left) # Add it to the queue
         if root.right: # If the root has right child
-            q.enqueue(root.right) # Add it to the queue
+            q.insert(0, root.right) # Add it to the queue
 
 LevelOrder(head)
